b1635313bae175d246b6137d18f05ab8b06411c1
kenmikanmi <kenh5526@gmail.com>
2018-08-05 01:47:25 +0900

    devide visualization func. into 2, former is for 2d, later for 3d

--- tag_dist.py
+++ tag_dist.py
@@ -35,24 +35,12 @@ def io_test():
 
     print(gaussian_KL(p,q))
 
-if __name__ == "__main__":
-    features = []
+def pca_visualization_3d():
+    features = np.empty([0, 128])
     model = load_model.model
     #model = stylenet.modelA
     model = model.cuda()
     model.train(False)
-    """
-    tensors = sampler.tag_sampling(50, 1)
-    print(tensors.shape)
-    features.append(model.forward(tensors.cuda()).cpu().detach().numpy())
-    features = np.array(features)[0]
-    print(features.shape)
-
-    pca = PCA(n_components=2)
-    x_pca = pca.fit_transform(features)
-    plt.scatter(x_pca[:,0],x_pca[:,1])
-    plt.show()
-    """
 
     plotlist = [37,41,65]
     itemlist = ["dress","jeans","belt"]
@@ -61,15 +49,47 @@ if __name__ == "__main__":
     ax = Axes3D(fig)
 
     for i in plotlist:
-        features = []
-        for j in range(10):
+        for j in range(30):
             tensors = sampler.tag_sampling(80, i)
-            features.append(model.forward(tensors.cuda()).cpu().detach().numpy())
+            features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
+        print(features.shape)
 
-        features = np.array(features)[0]
-        pca = PCA(n_components=3)
-        x_pca = pca.fit_transform(features)
-        ax.scatter3D(x_pca[:,0], x_pca[:,1], x_pca[:,2])
-        ax.legend(itemlist)
+    colors = ["r","g","b"]
+    label = [colors[int(x/8000)] for x in range(24000)]
+
+    features = np.array(features)
+    pca = PCA(n_components=3)
+    x_pca = pca.fit_transform(features)
+    ax.scatter3D(x_pca[:,0], x_pca[:,1], x_pca[:,2],c = label)
+    ax.legend(itemlist)
 
     plt.show()
+
+def pca_visualization_2d():
+    features = np.empty([0, 128])
+    model = load_model.model
+    #model = stylenet.modelA
+    model = model.cuda()
+    model.train(False)
+
+    plotlist = [37,41,65]
+    itemlist = ["dress","jeans","belt"]
+
+    for i in plotlist:
+        for j in range(30):
+            tensors = sampler.tag_sampling(80, i)
+            features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
+        print(features.shape)
+
+    colors = ["r","g","b"]
+    label = [colors[int(x/2400)] for x in range(7200)]
+
+    features = np.array(features)
+    pca = PCA(n_components=2)
+    x_pca = pca.fit_transform(features)
+    plt.scatter(x_pca[:,0], x_pca[:,1],c = label, alpha = 0.25)
+    plt.legend(itemlist)
+    plt.show()
+
+if __name__ == "__main__":
+    pca_visualization_2d()
