6d3bdb441f460f463b00c57636d6b81346b4dda6
kenmikanmi <kenh5526@gmail.com>
2018-08-06 22:25:28 +0900

    add covarience computation

--- tag_dist.py
+++ tag_dist.py
@@ -4,6 +4,7 @@ import image_sampling as sampler
 import stylenet
 import load_model   # temporal import
 from torch import Tensor
+from math import log,pi
 
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -38,7 +39,6 @@ def io_test():
 def pca_visualization_3d():
     features = np.empty([0, 128])
     model = load_model.model
-    #model = stylenet.modelA
     model = model.cuda()
     model.train(False)
 
@@ -84,12 +84,59 @@ def pca_visualization_2d():
     colors = ["r","g","b"]
     label = [colors[int(x/2400)] for x in range(7200)]
 
-    features = np.array(features)
     pca = PCA(n_components=2)
     x_pca = pca.fit_transform(features)
-    plt.scatter(x_pca[:,0], x_pca[:,1],c = label, alpha = 0.25)
+    plt.scatter(x_pca[:,0], x_pca[:,1],c = label, alpha = 0.3, marker = ".")
     plt.legend(itemlist)
     plt.show()
 
+def compute_gaussian(tag_idx):
+    features = np.empty([0, 128])
+    model = load_model.model
+    #model = stylenet.modelA
+    model = model.cuda()
+    model.train(False)
+
+    for i in tag_idx:
+        for j in range(15):
+            tensors = sampler.tag_sampling(80, tag_idx)
+            features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
+        print(features.shape)
+    """
+    pca = PCA(n_components=2)
+    x_pca = pca.fit_transform(features)
+    """
+    f_list = []
+    m_list = []
+    c_list = []
+    for i in range(len(tag_idx)):
+        f_list.append(features[i*80:(i+1)*80])
+        m_list.append(np.average(features[i*80:(i+1)*80], axis = 0))
+        c_list.append(np.cov(features[i*80:(i+1)*80].T)+ 0.00001*np.eye(128))
+
+    return m_list, c_list
+
+def compute_entropy(Cov):
+    #print("det|cov| is {}".format(np.linalg.det(Cov)))
+    return 0.5*(log(np.linalg.det(Cov))+128*(log(2*pi)+1))
+
+def get_tagNo(tag_list):
+    """
+    in: tag
+    """
+
+
 if __name__ == "__main__":
-    pca_visualization_2d()
+    #pca_visualization_2d()
+    tag_list = [37,41,55]
+    tag_name = ["dress","jeans","hat"]
+
+    m_list, c_list = compute_gaussian(tag_list)
+
+    for t,c in zip(tag_name, c_list):
+        print("{}:{}".format(t, np.linalg.det(c)))
+        print("{}:{}".format(t, compute_entropy(c)))
+
+    #for i, j in zip(m_list, c_list):
+    #    print("*****m,c*****")
+    #    print(i,j)
