93e262e9c953af65ab7f1c95a81687e7f2a16145
kenmikanmi <kenh5526@gmail.com>
2018-08-09 01:57:47 +0900

    add covariance computation for each tag

--- tag_dist.py
+++ tag_dist.py
@@ -5,11 +5,19 @@ import stylenet
 import load_model   # temporal import
 from torch import Tensor
 from math import log,pi
+from collections import OrderedDict
 
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 from mpl_toolkits.mplot3d import Axes3D
+
+tag_dict = OrderedDict()
+with open("./labels.txt") as f:
+    lines = f.readlines()
+    for idx,e in enumerate(lines):
+        e = e.strip()
+        tag_dict[idx] = e
 
 def gaussian_KL(p,q):
     """
@@ -76,7 +84,7 @@ def pca_visualization_2d():
     itemlist = ["dress","jeans","belt"]
 
     for i in plotlist:
-        for j in range(30):
+        for j in range(10):
             tensors = sampler.tag_sampling(80, i)
             features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
         print(features.shape)
@@ -91,6 +99,9 @@ def pca_visualization_2d():
     plt.show()
 
 def compute_gaussian(tag_idx):
+    """
+    DEPRECATED
+    """
     features = np.empty([0, 128])
     model = load_model.model
     #model = stylenet.modelA
@@ -98,10 +109,10 @@ def compute_gaussian(tag_idx):
     model.train(False)
 
     for i in tag_idx:
-        for j in range(15):
+        for j in range(30):
             tensors = sampler.tag_sampling(80, tag_idx)
             features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
-        print(features.shape)
+        #print(features.shape)
     """
     pca = PCA(n_components=2)
     x_pca = pca.fit_transform(features)
@@ -116,17 +127,26 @@ def compute_gaussian(tag_idx):
 
     return m_list, c_list
 
+def compute_gaussian2(tag_idx):
+    features = np.empty([0, 128])
+    model = load_model.model
+    model = model.cuda()
+    model.train(False)
+
+    for j in range(15):
+        tensors = sampler.tag_sampling(80, tag_idx)
+        features = np.append(features, model.forward(tensors.cuda()).cpu().detach().numpy(), axis = 0)
+
+    mean = np.average(features[i*80:(i+1)*80], axis = 0)
+    cov = np.cov((features[i*80:(i+1)*80].T))+ 0.00001*np.eye(128)
+
+    return mean, cov
+
 def compute_entropy(Cov):
     #print("det|cov| is {}".format(np.linalg.det(Cov)))
     return 0.5*(log(np.linalg.det(Cov))+128*(log(2*pi)+1))
 
-def get_tagNo(tag_list):
-    """
-    in: tag
-    """
-
-
-if __name__ == "__main__":
+def io_test2():
     #pca_visualization_2d()
     tag_list = [37,41,55]
     tag_name = ["dress","jeans","hat"]
@@ -137,6 +157,8 @@ if __name__ == "__main__":
         print("{}:{}".format(t, np.linalg.det(c)))
         print("{}:{}".format(t, compute_entropy(c)))
 
-    #for i, j in zip(m_list, c_list):
-    #    print("*****m,c*****")
-    #    print(i,j)
+if __name__ == "__main__":
+
+    for i in range(66):
+        m, c = compute_gaussian2(i)
+        print("{}:{}".format(tag_dict[i], np.linalg.det(c)))
