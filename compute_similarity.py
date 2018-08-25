d8ef069a49dd63ad948ee82f95c942c338542622
kenmikanmi <kenh5526@gmail.com>
2018-08-26 02:11:48 +0900

    Modified for Fashion550k

--- compute_similarity.py
+++ compute_similarity.py
@@ -1,96 +1,67 @@
 # coding: utf-8
-
 import random
 import numpy as np
 
-def simlarity(img1, img2):
+f_wei = open("weights.txt","r")
+weights = []
+
+for line in f_wei:
+    line = line.strip("\n")
+    line = line.split("\t")
+    weights.append(int(line[1]))
+
+label_array = np.load("./noisy.npy")
+
+def compute_simlarity(img1, img2):
     union_score = 0
     sum_score = 0
+    item_size = label_array.shape[1]
 
     # 要素が１であるインデックスを抽出
-    index1 = np.where(data[img1]==1)
-    index2 = np.where(data[img2]==1)
+    index1 = np.where(label_array[img1]==1)[0]
+    index2 = np.where(label_array[img2]==1)[0]
 
-    # img1とimg2に出現するアイテムのリストを作る
-    img1_items = []
-    img2_items = []
-    comm_items = []
+    common = set(index1)&set(index2)
+    one_side = (set(index1)|set(index2)) - common
 
-    item_count = 0
-    for lis in metalist:
-        if len(set(index1[0])&set(lis))!=0:
-            img1_items.append(item_count)
-        if len(set(index2[0])&set(lis))!=0:
-            img2_items.append(item_count)
-        if len(set(index1[0])&set(index2[0])&set(lis))!=0:
-            comm_items.append(item_count)
-        item_count+=1
+    inter = 0
+    union = 0
 
-    for i in comm_items:
-        union_score += (weights[i] + 2)
-    for i in set(img1_items)&set(img2_items)-set(comm_items):
-        union_score += weights[i]
-    for i in set(img1_items)|set(img2_items):
-        sum_score += (weights[i] + 2)
+    for i in common:
+        union += weights[i]
+        inter += weights[i]
 
-    return union_score/sum_score
+    for i in one_side:
+        union += weights[i]
 
-if __name__ == "__main__":
-    """
-    f1 = open("feat/colours.txt")
-    f2 = open("feat/single_unique.txt")
+    #print("{}, {}".format(inter, union))
+    if union == 0:
+        return -1
 
-    count = 0
-    sublist = []
-    metalist = []
+    return inter/union
 
-    line1 = f1.readlines()
-    line2 = f2.readlines()
-
-    for i in line2:
-        for j in line1:
-            if i.strip() in j.strip():
-                sublist.append(count)
-            count += 1
-
-        count = 0
-        metalist.append(sublist)
-        sublist = []
-    """
-
-    f_wei = open("weights.txt","r")
-
-    weights = []
-    values = [1,2,3,4,5]
-    modifed = [0,2,4,8,10]
-
-    for line in f_wei:
-        line = line.strip("\n")
-        line = line.split("\t")
-        weights.append(line[1])
-
-    data = np.load("./noisy.npy")
-
-    SIZE = 407772
-    PAIRS = 100000000000000000
+def sampling():
+    SIZE = 407771
 
     # compute similarity of images and save image triplet
-    for i in range(PAIRS):
+    while(True):
         # random sampling
         id1 = random.randint(0, SIZE)
         id2 = random.randint(0, SIZE)
         id3 = random.randint(0, SIZE)
 
-        sim = simlarity(id1,id2)
+        sim = compute_simlarity(id1,id2)
 
         if id1 != id2 and sim > 0.75:
             sim_neg = 0
-            while(sim_neg > 0.05):
+            while(sim_neg > 0.05 or sim_neg < 0):
                 id3 = random.randint(0, SIZE)
-                sim_neg = simlarity(id1,id3)
+                sim_neg = compute_simlarity(id1,id3)
 
             s = str(id1)+","+str(id2)+","+str(id3)+","+str(round(sim,5))+","+str(round(sim_neg,5))
-            #print(s)
-            f_ids_and_sims = open("triplet_v3.csv","a")
+            f_ids_and_sims = open("triplet.csv","a")
             f_ids_and_sims.write(s+"\n")
             f_ids_and_sims.close()
+
+if __name__ == "__main__":
+    sampling()
