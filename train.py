7049bd2ca76c040f57682ab756761c1747432420
kenmikanmi <kenh5526@gmail.com>
2018-09-25 00:41:34 +0900

    add binary_cross_entropy loss

--- train.py
+++ train.py
@@ -24,6 +24,9 @@ import random
 import csv
 
 cur_path = os.getcwd()
+sum_w = cs.sum_w
+print(sum_w , 'sum_w')
+N_tags = 66
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -34,21 +37,6 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 def triplet_loss(feat, sim, use_proposed = True):
-    alpha = 0.1
-
-    dist_pos = torch.norm(feat[0] - feat[1], 2)
-    dist_neg = torch.norm(feat[0] - feat[2], 2)
-
-    reg_dist_pos = exp(dist_pos)/(exp(dist_pos) + exp(dist_neg))
-    reg_dist_neg = exp(dist_neg)/(exp(dist_pos) + exp(dist_neg))
-
-    if(use_proposed):
-        loss_t = (((1 - sim[0]*alpha) - reg_dist_pos)**2 + (1 - reg_dist_neg)**2)/2
-        return loss_t
-    else:
-        return (reg_dist_pos)**2
-
-def triplet_cross_entropy_loss(feat, sim, use_proposed = True):
     alpha = 0.01
 
     dist_pos = torch.norm(feat[0] - feat[1], 2)
@@ -57,8 +45,25 @@ def triplet_cross_entropy_loss(feat, sim, use_proposed = True):
     reg_dist_pos = exp(dist_pos)/(exp(dist_pos) + exp(dist_neg))
     reg_dist_neg = exp(dist_neg)/(exp(dist_pos) + exp(dist_neg))
 
-    loss_t = -(1-sim[0]*alpha)*torch.log(reg_dist_pos)
-    return loss_t
+    if(use_proposed):
+        loss_t = (((1 - sim[0])*alpha - reg_dist_pos)**2)
+        return loss_t
+    else:
+        return (reg_dist_pos)**2
+
+def triplet_cross_entropy_loss(feat, sim, use_proposed = True):
+    alpha = 0.1
+    w1 = 0.8
+    w2 = 1 - w1
+
+    dist_pos = torch.norm(feat[0] - feat[1], 2)
+    dist_neg = torch.norm(feat[0] - feat[2], 2)
+
+    reg_dist_pos = exp(dist_pos)/(exp(dist_pos) + exp(dist_neg))
+    reg_dist_neg = exp(dist_neg)/(exp(dist_pos) + exp(dist_neg))
+
+    loss_t = -alpha*(1-sim[0])*torch.log(reg_dist_pos)*w1 -1*torch.log(reg_dist_neg)*w2
+    return loss_t/2
 
 def classfi_loss(log_y, target, use_proposed = True):
     loss_c = 0
@@ -77,6 +82,20 @@ def classfi_loss(log_y, target, use_proposed = True):
             loss_c += -1*log_y[t]   # cross entropy
         loss_c = loss_c/N
         return loss_c
+
+def binary_classfi_loss(conf, target_npy, use_proposed = True):
+    loss_c = 0
+    if(use_proposed):
+        for t in range(N_tags):
+            y_k = (target_npy[t] - 0.5) * 2
+            loss_c += w[t]*torch.log(1 + torch.exp(-int(y_k) * conf[t]))    # cross entropy
+        return loss_c/sum_w
+
+    else:
+        for t in range(N_tags):
+            y_k = (target[t] - 0.5) * 2
+            loss_c += torch.log(1 + torch.exp(-int(y_k) * conf[t]))    # cross entropy
+        return loss_c/N_tags
 
 def insert_trained_weight(model, path_to_weight = "linear_weight.pth"):
 
@@ -98,11 +117,11 @@ def save_loss(loss_prog, add = ""):
         f_loss.write(str(i)+'\n')
     f_loss.close()
 
-def send_prog_image(add = ""):
+def send_prog_image(add = "", split = 100):
     with open('loss_progress'+add+'.txt','r') as f:
         lines = f.readlines()
 
-    num_split = 100
+    num_split = split
     sum_e = 0
     mean = []
 
@@ -143,24 +162,29 @@ def load_training_ids(batch_size, use_proposed):
 if __name__ == "__main__":
     w = cs.weights
     use_proposed = True
-    do_classification = False
+    do_classification = True
 
     # load model
     if do_classification:
-        model = stylenet.get_model()
-        model = insert_trained_weight(model)
+        model = stylenet.Stylenet()
+        model.load_state_dict(torch.load('linear_weight_softmax_118000.pth'))
+        forward = model.forward_
     else:
         model = load_model.model
         model.load_state_dict(torch.load('stylenet.pth'))
-        model = model.cuda()
+        forward = model.forward
+
+    model.cuda()
+    model.train()
 
     # learning settings
-    learning_rate = 0.01
-    epochs = 1000
-    optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, amsgrad = True)
-    batch_size = 16
-    alpha_c = 0.01
+    learning_rate = 1e-4
+    epochs = 500
+    #optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, amsgrad = True)
+    batch_size = 22
+    alpha_c = 1
+    split = 500
 
     max_score = 0
     loss_progress = []
@@ -169,36 +193,56 @@ if __name__ == "__main__":
     #optimizer.load_state_dict(torch.load('./result/params/optim_lr001_clas=True_epoch0_iter10000_5.pth'))
 
     for epoch in range(epochs):
+
         row, batchs = load_training_ids(batch_size, use_proposed)
         print(len(batchs))
 
         for o, batch in enumerate(batchs):
             loss = 0
-            model.train(True)
+            model.train()
 
-            # img, sim are list where i th column holds i th triplet
-            img, sim, target = triplet_sampling(row, batch, do_classification = True)
+            if(o% split == 0 and o != 0):
+                save_loss(loss_progress)
+                loss_progress = []
+                path_to_fig = send_prog_image()
+                ln.send_image_(path_to_fig, 'loss progress')
+
+                model_path = "./result/params/prams_lr001_clas=True_epoch{}_iter{}_7.pth".format(epoch, o)
+                optim_path = "./result/params/optim_lr001_clas=True_epoch{}_iter{}_7.pth".format(epoch, o)
+                torch.save(model.state_dict(), model_path)
+                torch.save(optimizer.state_dict(), optim_path)
+
+                temp_score = test.test(model_path, do_classification)
+                ln.notify("{} {}".format(str(temp_score), model_path))
+
+                print("{} {}".format(temp_score, max_score))
+
+
+
+            # img, sim are list: i th column holds i th triplet
+            img, sim, target, target_npy = triplet_sampling(row, batch)
             batch_count = 0
 
             feat = []
             pred = []
             for j in range(3):
                 if do_classification:
-                    p, f = model.forward(Variable(img[j]).cuda())
+                    f, p = forward(Variable(img[j]).cuda())
                     if(j == 2):
                         pred.append(p)
                 else:
-                    f = model.forward(Variable(img[j]).cuda())
+                    f = forward(Variable(img[j]).cuda())
                 feat.append(f)
 
             # LOSS COMPUTING
             for i in range(batch_size):
+                # loss += triplet_cross_entropy_loss([feat[0][i],feat[1][i],feat[2][i]], (sim[0][i],sim[1][i]), use_proposed)
                 loss += triplet_loss([feat[0][i],feat[1][i],feat[2][i]], (sim[0][i],sim[1][i]), use_proposed)
                 if(do_classification):
-                    loss += alpha_c * classfi_loss(pred[0][i], target[i], use_proposed)
-                batch_count += 1
+                    loss += alpha_c * binary_classfi_loss(pred[0][i], target_npy[i], use_proposed)
 
-            del feat
+                #batch_count += 1
+
             loss = loss/batch_size
             print("{} {:.4f}".format(o, loss.cpu().detach().numpy()))
             loss_progress.append("{:.4f}".format(loss.cpu().detach().numpy()))
@@ -206,19 +250,3 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-            if(o%1000 == 0 and o != 0):
-                save_loss(loss_progress)
-                loss_progress = []
-                path_to_fig = send_prog_image()
-                ln.send_image_(path_to_fig, 'loss progress')
-
-                model_path = "./result/params/prams_lr001_clas=True_epoch{}_iter{}_5.pth".format(epoch, o)
-                optim_path = "./result/params/optim_lr001_clas=True_epoch{}_iter{}_5.pth".format(epoch, o)
-                torch.save(model.state_dict(), model_path)
-                torch.save(optimizer.state_dict(), optim_path)
-
-                temp_score = test.test(model_path)
-                ln.notify("{} {}".format(str(temp_score), model_path))
-
-                print("{} {}".format(temp_score, max_score))
